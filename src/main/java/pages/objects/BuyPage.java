9009e8363893636cbb401092e30bdd3ce69fd673
Alexander Mizyukin <a.mizyukin@mail.ru>
2022-03-22 20:23:24 +0300

    init

--- src/main/java/pages/objects/BuyPage.java
+++ src/main/java/pages/objects/BuyPage.java
@@ -1,20 +1,24 @@
 package pages.objects;
 
 import com.codeborne.selenide.ClickOptions;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.ClassOrderer;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.Objects;
 
 public class BuyPage {
 
     private static BuyPage buyPage;
 
-    private SelenideElement categorySetCheckBox;
-    private SelenideElement brandSetCheckBox;
+
     private SelenideElement priceSetMin;
     private SelenideElement priceSetMax;
-    private SelenideElement itemSetButton;
+    public SelenideElement itemSetButton;
+    public SelenideElement categorySetCheckBox;
+    public SelenideElement brandSetCheckBox;
+    public SelenideElement paginationSet;
 
     @FindBy(className = "Category")
     private SelenideElement productCategory;
@@ -28,21 +32,17 @@ public class BuyPage {
     @FindBy(className = "ItemList")
     private SelenideElement itemList;
 
-
-    public void setCategory (ClickOptions category) {
-        buyPage.categorySetCheckBox.click(category);
-    }
-
-    public void setBrand (ClickOptions brand) {
-        buyPage.brandSetCheckBox.click(brand);
-    }
-
     public void setPrice (int min, int max) {
         buyPage.priceSetMin.setValue(String.valueOf(min));
         buyPage.priceSetMin.setValue(String.valueOf(max));
     }
 
-    public void setItem (ClickOptions item) {
-        buyPage.itemSetButton.click(item);
+    public static BuyPage getBuyPage() {
+        if (Objects.isNull(buyPage)) buyPage = Selenide.page(new BuyPage());
+        return buyPage;
+    }
+
+    public void setPaginationPage(ClickOptions count) {
+        buyPage.paginationSet.click(count);
     }
 }
--- src/main/java/pages/step/Steps.java
+++ src/main/java/pages/step/Steps.java
@@ -1,6 +1,24 @@
 package pages.step;
 
+import com.codeborne.selenide.ClickOptions;
+import com.codeborne.selenide.SelenideElement;
+import pages.objects.BuyPage;
+
 public class Steps {
 
+    private BuyPage buyPage;
 
+    public Steps() {
+        buyPage = BuyPage.getBuyPage();
+    }
+
+    public void setCategory (ClickOptions category) {
+        buyPage.categorySetCheckBox.click(category);
+    }
+
+    public void setBrand (ClickOptions brand) {
+        buyPage.brandSetCheckBox.click(brand);
+    }
+
+    public void setItemInList (ClickOptions item) {buyPage.itemSetButton.click(item);}
 }
